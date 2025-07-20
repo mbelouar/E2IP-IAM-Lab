30446ad0895d6f38b1cd333a2218bd92b6da7980
mbelouar <simobelouarraq5@gmail.com>
2025-07-20 13:32:14 +0100

    start from the beginning

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout/', views.logout_view, name='logout_view'),
-]
--- create_init_files.sh
+++
@@ -1,2 +0,0 @@
-touch /Users/mohammedbelouarraq/Desktop/E2IP-IAM-Lab/core/management/__init__.py
-touch /Users/mohammedbelouarraq/Desktop/E2IP-IAM-Lab/core/management/commands/__init__.py
